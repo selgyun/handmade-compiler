e5cf07f1b915ad52437b5c56c7615e87fe0c6ac5
HAN TAE GYUN <kindkiz@naver.com>
2021-04-05 23:28:09 +0900

    feat: Add tokens to transtable

--- main.py
+++ main.py
@@ -14,8 +14,66 @@ singleChar = DFA(
     table.get_final_states("SINGLE_CHARACTER"),
 )
 
+variableType = DFA(
+    "variable type",
+    'i'+'n'+'t'+'f'+'l'+'o'+'a'+'b'+'e'+'c'+'h'+'r'+'S'+'g',
+    table.table["VARIABLE_TYPE"],
+    0,
+    table.get_final_states("VARIABLE_TYPE"),
+)
+
+literalString = DFA(
+    "literal string",
+    string.digits + string.whitespace + string.ascii_letters + '"',
+    table.table["LITERAL_STRING"],
+    0,
+    table.get_final_states("LITERAL_STRING"),
+)
+
+test_public = DFA(
+    "public",
+    'p' + 'u' + 'b' + 'l' + 'i' + 'c',
+    table.table["KEYWORD"],
+    0,
+    table.get_final_states("KEYWORD"),
+)
+
 singleChar.run("'")
 singleChar.run("c")
 singleChar.run("'")
 
+variableType.run('i')
+variableType.run('n')
+variableType.run('t')
+variableType.reset()
+variableType.run('b')
+variableType.run('o')
+variableType.run('o')
+variableType.run('l')
+variableType.run('e')
+variableType.run('a')
+variableType.run('n')
+
+literalString.run('"')
+literalString.run('h')
+literalString.run('e')
+literalString.run('l')
+literalString.run('l')
+literalString.run('o')
+literalString.run(' ')
+literalString.run('w')
+literalString.run('o')
+literalString.run('r')
+literalString.run('l')
+literalString.run('d')
+literalString.run('"')
+
+test_public.run('p')
+test_public.run('u')
+test_public.run('b')
+test_public.run('l')
+test_public.run('i')
+test_public.run('c')
+
 print(singleChar.isDone())
+print(variableType.isDone())
--- transtable.py
+++ transtable.py
@@ -47,11 +47,153 @@ class TransitionTable:
                 4: {"'": 5},
                 5: {},
                 "final": [5],
+            },
+            # int, char, boolean, String
+            "VARIABLE_TYPE": {
+                0: {"i": 1, "c": 3, "b": 6, "S": 12},
+                1: {"n": 2},
+                2: {"t": 17},
+                3: {"h": 4},
+                4: {"a": 5},
+                5: {"r": 17},
+                6: {"o": 7},
+                7: {"o": 8},
+                8: {"l": 9},
+                9: {"e": 10},
+                10: {"a": 11},
+                11: {"n": 17},
+                12: {"t": 13},
+                13: {"r": 14},
+                14: {"i": 15},
+                15: {"n": 16},
+                16: {"g": 17},
+                17: {},
+                "final": [17],
+            },
+
+            "SIGNED_INTEGER": {
+                0: {"0": 3, "-": 1, string.digits.replace('0', ''): 2},
+                1: {string.digits.replace('0', ''): 2},
+                2: {string.digits: 2},
+                3: {},
+                "final": [2, 3],
+            },
+            # true, false
+            "BOOLEAN_STRING": {
+                0: {"t": 1, "f": 3},
+                1: {"r": 2},
+                2: {"u": 6},
+                3: {"a": 4},
+                4: {"l": 5},
+                5: {"s": 6},
+                6: {"e": 7},
+                7: {},
+                "final": [7],
+            },
+
+            "WHITE_SPACE": {
+                0: {string.whitespace: 1},
+                1: {string.whitespace: 1},
+                "final": [1],
+            },
+
+            "ARITHMETIC_OPERATOR": {
+                0: {"+": 1, "*": 1, "-": 1, "/": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "LOGICAL_OPERATOR": {
+                0: {"&": 1, "|": 2},
+                1: {"&": 3},
+                2: {"|": 3},
+                3: {},
+                "final": [3],
+            },
+
+            "ASSIGNMENT_OPERATOR": {
+                0: {"=": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "COMPARISON_OPERATOR": {
+                0: {"<": 1, ">": 1, "=": 2, "!": 2},
+                1: {"=": 3},
+                2: {"=": 3},
+                3: {},
+                "final": [1, 3]
+            },
+
+            "TERMINATING_SYMBOL": {
+                0: {";": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "LPAREN": {
+                0: {"(": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "RPAREN": {
+                0: {")": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "LBRACE": {
+                0: {"{": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "RBRACE": {
+                0: {"}": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "LBRANKET": {
+                0: {"[": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "RBRANKET": {
+                0: {"]": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "COMMA": {
+                0: {",": 1},
+                1: {},
+                "final": [1],
+            },
+
+            "LITERAL_STRING": {
+                0: {'"': 1},
+                1: {string.ascii_letters: 2, string.digits: 3, string.whitespace: 4, '"': 5},
+                2: {string.ascii_letters: 3, string.digits: 4, string.whitespace: 1, '"': 5},
+                3: {string.ascii_letters: 4, string.digits: 1, string.whitespace: 2, '"': 5},
+                4: {string.ascii_letters: 1, string.digits: 2, string.whitespace: 3, '"': 5},
+                5: {},
+                "final": [5],
+            },
+
+            "IDENTIFIER": {
+                0: {"_": 1, string.ascii_letters: 2},
+                1: {string.ascii_letters: 2, string.digits: 3, "_": 1},
+                2: {string.ascii_letters: 3, string.digits: 1, "_": 2},
+                3: {string.ascii_letters: 1, string.digits: 2, "_": 3},
+                "final": [1, 2, 3]
             }
         }
 
         self.table["KEYWORD"] = build_transition_table_with_lexemes(
-            ["if", "else", "while"]
+            ["if", "else", "while", "class", "return", "public"]
         )
 
     def get_final_states(self, name_of_rule):
@@ -65,4 +207,3 @@ if __name__ == "__main__":
     print("======================================================")
     print(table.get_final_states("KEYWORD"))
     print("======================================================")
-    
