382891cfba0775d378b0eb760749c625f0ffb555
HAN TAE GYUN <kindkiz@naver.com>
2021-04-06 19:56:49 +0900

    feat: Create test.java
    test file written in simplified java language.

---
+++ test.java
@@ -0,0 +1,22 @@
+public class test {
+
+	public static int main(String[] args) {
+		int _aa_a = -1;
+		char b_b123 = '&';
+		boolean __cc = true;
+		String d__d_0 = "Hello World";
+		
+		if(_aa_a <= 0) {
+			_aa_a = 3923 + 41 - -1- -1- 1-1;
+		}else if(_aa_a == 0){
+			b_b123 = '3';
+		}
+		
+		while(__cc) {
+			__cc = false;
+		}
+		
+		return 0;
+	}
+
+}
